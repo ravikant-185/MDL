74f4c5acd226e1eb0e5077ec12ae5f7af2277efa
Ravikant Pandey <ravikant185@example.com>
2025-07-31 16:48:19 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './components/Navbar/avbar';
+import Banner from './components/Banner/Banner';
+import Footer from './components/Footer/Footer';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './banner.css';
+
+const Banner = () => {
+  return (
+    <section className="banner-section">
+      <div className="banner-text">
+        <h1><span class="break-word">Human</span> stories & ideas</h1>
+        <p>A place to read, write, and deepen your understanding</p>
+        <button>Start reading</button>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
