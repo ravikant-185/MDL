173d3816d370f2ec4676cf6c7f7eaeebe5a08119
Ravikant Pandey <ravikant185@example.com>
2025-07-31 21:17:15 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './components/Navbar/avbar';
+import Navbar from './components/Navbar/Navbar';
 import Banner from './components/Banner/Banner';
 import Footer from './components/Footer/Footer';
 
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
   return (
     <section className="banner-section">
       <div className="banner-text">
-        <h1><span class="break-word">Human</span> stories & ideas</h1>
+        <h1><span className="break-word">Human</span> stories & ideas</h1>
         <p>A place to read, write, and deepen your understanding</p>
         <button>Start reading</button>
       </div>
